84deb53564d6d8bd0b3bdba421adf5228127db34
Hsn <ihasan.azimi@gmail.com>
2024-03-06 20:53:25 +0330

    temp

--- app/src/main/java/com/example/composepracticeproject/Basic_Lazy_Column_Row_Smaples.kt
+++ app/src/main/java/com/example/composepracticeproject/Basic_Lazy_Column_Row_Smaples.kt
@@ -1,8 +1,6 @@
 package com.example.composepracticeproject
 
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
+import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -14,12 +12,14 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,10 +30,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-val CitiesList = mutableListOf("Tehran", "Alborz", "Tabriz", "Shiraz", "Mashhad")
+
+@Stable
+data class CityEntity(val name: String, var expanded: Boolean = false)
 
 val myTextStyle =
-    TextStyle(fontSize = 18.sp, color = Color.White, shadow = Shadow(color = Color.Gray))
+    TextStyle(
+        fontSize = 18.sp,
+        color = Color.White,
+        shadow = Shadow(color = Color.Gray)
+    )
 
 val listModifier = Modifier
     .fillMaxSize()
@@ -41,51 +47,62 @@ val listModifier = Modifier
     .padding(10.dp)
 
 
-
 @Composable
 fun SimpleListView() {
 
-    val itemExpanded = remember { mutableStateOf(false) }
-//    val extraPadding = if (itemExpanded.value) 48.dp else 0.dp
-    val extraPadding by animateDpAsState(
-        if(itemExpanded.value) 48.dp else 0.dp, label = ""
-    )
+
+    var myList by remember {
+        mutableStateOf<List<CityEntity>>(
+            arrayListOf(
+                CityEntity("Tehran", false),
+                CityEntity("Alborz", false),
+                CityEntity("Tabriz", false),
+                CityEntity("Shiraz", false),
+                CityEntity("Mashhad", false),
+            )
+        )
+    }
+
+//    val itemExpanded = remember { mutableStateOf(false) }
+//
+////  todo  val extraPadding = if (itemExpanded.value) 48.dp else 0.dp'
+//
+//    val extraPadding by animateDpAsState(
+//        if(itemExpanded.value) 48.dp else 0.dp, label = ""
+//    )
 
     LazyColumn(modifier = listModifier) {
-        items(CitiesList) {
-            Surface {
-                Box(modifier = Modifier
+        items(myList) { city ->
+            Box(
+                modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = extraPadding)) {
+                    .padding(horizontal = 16.dp, vertical = if (city.expanded) 48.dp else 0.dp)
+            ) {
 
-                    Divider()
+                Text(
+                    text = city.name,
+                    style = myTextStyle,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .background(if (city.expanded) Color.Black else Color.Red)
+                        .align(Alignment.CenterStart)
+                        .padding(4.dp)
+                )
 
-                    Text(
-                        text = it,
-                        style = myTextStyle,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .background(Color.Black)
-                            .align(Alignment.CenterStart)
-                            .padding(4.dp)
-                    )
-
-
-                    Button(
-                        onClick = {
-                            itemExpanded.value = !itemExpanded.value
-                        },
-                        border = BorderStroke(1.dp, Color.Black),
-                        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black),
-                        modifier = Modifier
-                            .align(Alignment.CenterEnd)
-                            .padding(vertical = 16.dp)
-                    ) {
-                        Text(if (itemExpanded.value) "Show Less" else "Show More")
-                    }
+                Button(
+                    border = BorderStroke(1.dp, Color.Black),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Black),
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(vertical = 16.dp),
+                    onClick = {
+                        Log.i("hasan", "obj: ${city}")
+                        city.expanded = city.expanded.not()
+                    }) {
+                    Text(if (city.expanded) "Less" else "More")
                 }
+                Divider()
             }
-
         }
     }
 }
@@ -95,5 +112,4 @@ fun SimpleListView() {
 @Preview(showSystemUi = true)
 fun BasicsRowAndColumnPreview() {
     SimpleListView()
-
 }
